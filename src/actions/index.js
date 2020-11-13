39e74a5089ac884d3f0c65e6eb3322ef8a04e399
Lilkgb <mcleanrs@comcast.net>
2020-11-13 10:11:26 -0800

    add firebase and initialize it

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import constants from './../constants';
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 const {types, firebaseConfig} = constants;
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 export const testFunction = () => ({
   type: types.TEST_FUNCTION
