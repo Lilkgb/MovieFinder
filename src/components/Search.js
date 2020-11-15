70daf1bc81ee5290c92c2a60f25484d195a366dd
Lilkgb <mcleanrs@comcast.net>
2020-11-15 11:43:04 -0800

    update styling and add search params

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Search(){
+
+    function updateParam(e){
+        setSearchParam(e);
+        setDropDown(!dropDown)
+    }
+
+    const [dropDown, setDropDown] = useState(false);
+    const [searchParam, setSearchParam] = useState("All");
+
     return(
         <div className="searchBar">
-            <button>ALL &#9660;</button>
-            <input placeholder="Search" />
+            <div className="searchContainer">
+                <button onClick={() => setDropDown(!dropDown)}>{searchParam} &#9660;</button>
+                <input placeholder="Search" />
+            </div>
+            <div className={`switch ${dropDown ? "dropDown" : "hide"}`} >
+                <p onClick={() => updateParam("All")}>All</p>
+                <p onClick={() => updateParam("Titles")}>Titles</p>
+                <p onClick={() => updateParam("Names")}>Names</p>
+                <p onClick={() => updateParam("Companies")}>Companies</p>
+            </div>
         </div>
     )
 }
